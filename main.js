fbba4d226e1edad5274b3b453640838317ab955e
Damien S <damienstephan07@gmail.com>
2020-10-06 19:49:08 +0200

    Use Vue.js 👀

--- main.js
+++ main.js
@@ -1,162 +1,60 @@
-function submit() {
-  let surname = document.querySelector('#Prénom').value;
-  document.querySelector('.surname').textContent = surname;
-
-
-  let name = document.querySelector('#Nom').value;
-  document.querySelector('.name').textContent = name;
-
-  let fixe = document.querySelector('#Fixe').value;
-
-  let mob = document.querySelector('#Mobile').value;
-
-
-  let email = document.querySelector('#Mail').value;
-  if (email !== '') {
-    document.querySelector('.mail').textContent = '✉ ' + email;
-  } else {
-    document.querySelector('.mail').textContent = '';
-  }
-
-  let adresse = document.querySelector('#Adresse').value;
-  if (adresse !== '') {
-    document.querySelector('.address').textContent = '📬 ' + adresse;
-  } else {
-    document.querySelector('.address').textContent = '';
-  }
-
-  mettreEnFormeLesDonnéesTel(fixe);
-  mettreEnFormeLesDonnéesTelMob(mob);
-
-  if (name === '' && surname === '' && email === '' && adresse === '' && mobile === '' && fixe === '') {
-    document.querySelector('.preview').style.visibility = "hidden";
-  } else {
-    document.querySelector('.preview').style.visibility = "visible";
-  }
-}
-
 const file = document.querySelector('#file');
-const img = document.querySelector('.picture')
+let img = document.querySelector('.picture');
 const btn = document.querySelector('#uploadBtn');
 
-btn.onclick = function(e){
-  file.click()
-}
+let app = new Vue({
+  el: 'main',
+  data: {
+    name: '',
+    surname: '',
+    fixe: '',
+    mobile: '',
+    mail: '',
+    adresse: '',
+    src: ''
+  },
+  methods: {
+    reset: function(){
+      this.name = '';
+      this.surname = '';
+      this.mail = '';
+      this.fixe = '';
+      this.mobile = '';
+      this.adresse = '';
+    },
+    upload: function(){
+      const photo = file.files[0];
 
-file.addEventListener('change', function(e){
-  const photo = this.files[0];
-
-  if (photo) {
-    const analyseur = new FileReader();
-    analyseur.readAsDataURL(photo);
-    analyseur.addEventListener('load', function(e){
-      img.setAttribute('src', this.result)
-      img.style.visibility = 'visible';
-      document.querySelector('.preview').style.visibility = "visible";
-    })
+      if (photo) {
+        const analyseur = new FileReader();
+        analyseur.readAsDataURL(photo);
+        analyseur.onload = function(e){
+          app.src = this.result;
+        }
+      }
+    },
+    click: function() {
+        file.click();
+    }
   }
 })
-/*
-███████ ██ ██   ██ ███████
-██      ██  ██ ██  ██
-█████   ██   ███   █████
-██      ██  ██ ██  ██
-██      ██ ██   ██ ███████
-*/
-function mettreEnFormeLesDonnéesTel(data) {
-  let données = data;
-  let prm = données.slice(0, 2);
-  let dxm = données.slice(2, 4);
-  let trm = données.slice(4, 6);
-  let qtm = données.slice(6, 8);
-  let cqm = données.slice(8);
 
-  let affichage = prm + " " + dxm + " " + trm + " " + qtm + " " + cqm;
-  if (document.querySelector('#Fixe').value !== '') {
-    document.querySelector('.telfix').textContent = '☎ Fixe : ' + affichage;
-    document.querySelector('.preview').style.visibility = "visible";
-  } else {
-    document.querySelector('.telfix').textContent = '';
-  }
-
-}
-/*
-███    ███  ██████  ██████  ██ ██      ███████
-████  ████ ██    ██ ██   ██ ██ ██      ██
-██ ████ ██ ██    ██ ██████  ██ ██      █████
-██  ██  ██ ██    ██ ██   ██ ██ ██      ██
-██      ██  ██████  ██████  ██ ███████ ███████
-*/
-
-function mettreEnFormeLesDonnéesTelMob(data) {
-  let données = data;
-  let prm = données.slice(0, 2);
-  let dxm = données.slice(2, 4);
-  let trm = données.slice(4, 6);
-  let qtm = données.slice(6, 8);
-  let cqm = données.slice(8);
-
-  let affichage = prm + " " + dxm + " " + trm + " " + qtm + " " + cqm;
-  if (document.querySelector('#Mobile').value !== '') {
-    document.querySelector('.telmobile').textContent = '📱 Mobile : ' + affichage;
-    document.querySelector('.preview').style.visibility = "visible";
-  } else {
-    document.querySelector('.telmobile').textContent = '';
-  }
-
-}
-/*
-██████  ███████ ███████ ███████ ████████
-██   ██ ██      ██      ██         ██
-██████  █████   ███████ █████      ██
-██   ██ ██           ██ ██         ██
-██   ██ ███████ ███████ ███████    ██
-*/
-
-function reset() {
-  document.querySelector('.name').textContent = "";
-  document.querySelector('.surname').textContent = "";
-  document.querySelector('.telfix').textContent = "";
-  document.querySelector('.telmobile').textContent = "";
-  document.querySelector('.address').textContent = "";
-  document.querySelector('.picture').setAttribute('src', '');
-  document.querySelector('.preview').style.visibility = "hidden";
-  document.querySelector('#Nom').value = '';
-  document.querySelector('#Prénom').value = '';
-  document.querySelector('#Fixe').value = '';
-  document.querySelector('#Mobile').value = '';
-  document.querySelector('#Mail').value = '';
-  document.querySelector('#Adresse').value = '';
-  document.querySelector('#Photo').value = '';
-}
-/*
-██████   ██████  ██      ██  ██████ ███████
-██   ██ ██    ██ ██      ██ ██      ██
-██████  ██    ██ ██      ██ ██      █████
-██      ██    ██ ██      ██ ██      ██
-██       ██████  ███████ ██  ██████ ███████
-*/
+// polices
 
 function police() {
   let choix = document.querySelector('select').value;
   document.querySelector('.preview').setAttribute('id', choix);
 };
-/*
-██████  ███████ ███    ██ ███████ ██████  ███████ ██████
-██       ██      ████   ██ ██      ██   ██ ██      ██   ██
-██   ███ █████   ██ ██  ██ █████   ██████  █████   ██████
-██    ██ ██      ██  ██ ██ ██      ██   ██ ██      ██   ██
-██████  ███████ ██   ████ ███████ ██   ██ ███████ ██   ██
-*/
+
+// génération
 
 function générerSignature() {
   let div = document.querySelector('.preview').innerHTML;
-  let index = div.indexOf('<img');
+  let index = div.indexOf('<div class="identity">');
   let indexA = div.indexOf('<a');
   let font = document.querySelector('select').value;
-  let signature = '<!DOCTYPE html> <head><meta charset="utf-8"></head><body>' + div.slice(index, indexA) + '<style>*{font-family: "' + font + '"} .picture {padding-right: 15px; max-width: 300px; max-height: 100px;}hr {border-color: #eb2f06;}' + '</body></html>'
+  let signature = '<!DOCTYPE html> <head><meta charset="utf-8"></head><body>' + div.slice(index, indexA) + '<style>*{font-family: "' + font + '"} .picture {padding-right: 15px; max-width: 100px; max-height: 100px;} .identity {display: flex; justify-content: flex-start; align-items: center;} hr {border-color: #eb2f06;}' + '</body></html>'
   console.info("La signature a bien été générée !");
-  console.log(signature);
 
   var element = document.querySelector('.dl');
 
@@ -164,13 +62,8 @@ function générerSignature() {
   element.setAttribute('href', href);
   element.setAttribute('download', 'Signature.html');
 };
-/*
-█████  ███    ██ ██ ███    ███  █████  ████████ ██  ██████  ███    ██ ███████
-██   ██ ████   ██ ██ ████  ████ ██   ██    ██    ██ ██    ██ ████   ██ ██
-███████ ██ ██  ██ ██ ██ ████ ██ ███████    ██    ██ ██    ██ ██ ██  ██ ███████
-██   ██ ██  ██ ██ ██ ██  ██  ██ ██   ██    ██    ██ ██    ██ ██  ██ ██      ██
-██   ██ ██   ████ ██ ██      ██ ██   ██    ██    ██  ██████  ██   ████ ███████
-*/
+
+// animations
 
 let myTitleText = 'Signature ✒';
 let myTitleArray = myTitleText.split('');
@@ -201,10 +94,3 @@ function loopH5() {
 }
 
 loopH5();
-
-//var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-/*if (isIE === true) {
-alert('Vous utilisez Internet Explorer... Ce n\'est pas très respectueux pour moi, qui ai développé ce site, d\'utiliser un navigateur préhistorique ! Bon, ça va pour cette fois ci, vous pouvez utiliser le site, mais essayez de passer à Chrome, Firefox, Edge, Opera... ');
-} else {
-}*/
